3b67d248d5af98609a4bb389b4182ac208026131
unadlib <unadlib@gmail.com>
2021-01-18 01:52:37 +0800

    refactor(type): refactor interface

---
+++ packages/fronts-bundler/src/interface.ts
@@ -0,0 +1,57 @@
+import { container } from 'webpack';
+
+export interface RemotesConfig {
+  /**
+   * Container locations from which modules should be resolved and loaded at runtime.
+   */
+  external: string | string[];
+  /**
+   * The name of the share scope shared with this remote.
+   */
+  shareScope?: string;
+}
+
+export interface DependencyConfig {
+  /**
+   * Container locations from which modules should be resolved and loaded at runtime.
+   */
+  external?: string | string[];
+  /**
+   * The name of the share scope shared with this remote.
+   */
+  shareScope?: string;
+  /**
+   * The host url for this remote.
+   */
+  host?: string;
+  /**
+   * The main filename for this remote.
+   */
+  main?: string;
+}
+
+export type ModuleFederationPluginOptions = ConstructorParameters<
+  typeof container.ModuleFederationPlugin
+>[0];
+
+export interface SiteConfig
+  extends Pick<
+    ModuleFederationPluginOptions,
+    Exclude<
+      keyof ModuleFederationPluginOptions,
+      'exposes' | 'filename' | 'remotes'
+    >
+  > {
+  /**
+   * The main filename of the container as relative path inside the `output.path` directory.
+   */
+  main?: string;
+  /**
+   * Modules that should be exposed by this container. When provided, property name is used as public name, otherwise public name is automatically inferred from request.
+   */
+  exports?: string[];
+  /**
+   * Container dependent locations and request scopes from which modules should be resolved and loaded at runtime. When provided, property name is used as request scope, otherwise request scope is automatically inferred from container dependent location.
+   */
+  dependencies?: Record<string, DependencyConfig | string>;
+}
--- packages/fronts-bundler/src/plugins.ts
+++ packages/fronts-bundler/src/plugins.ts
@@ -1,64 +1,13 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 import path from 'path';
 import { container, DefinePlugin } from 'webpack';
+import {
+  SiteConfig,
+  ModuleFederationPluginOptions,
+  RemotesConfig,
+} from './interface';
 
 const DEFAULT_DEPENDENCY_CONFIG_MAIN = 'remoteEntry.js';
-
-interface RemotesConfig {
-  /**
-   * Container locations from which modules should be resolved and loaded at runtime.
-   */
-  external: string | string[];
-  /**
-   * The name of the share scope shared with this remote.
-   */
-  shareScope?: string;
-}
-
-interface DependencyConfig {
-  /**
-   * Container locations from which modules should be resolved and loaded at runtime.
-   */
-  external?: string | string[];
-  /**
-   * The name of the share scope shared with this remote.
-   */
-  shareScope?: string;
-  /**
-   * The host url for this remote.
-   */
-  host?: string;
-  /**
-   * The main filename for this remote.
-   */
-  main?: string;
-}
-
-type ModuleFederationPluginOptions = ConstructorParameters<
-  typeof container.ModuleFederationPlugin
->[0];
-
-interface SiteConfig
-  extends Pick<
-    ModuleFederationPluginOptions,
-    Exclude<
-      keyof ModuleFederationPluginOptions,
-      'exposes' | 'filename' | 'remotes'
-    >
-  > {
-  /**
-   * The main filename of the container as relative path inside the `output.path` directory.
-   */
-  main?: string;
-  /**
-   * Modules that should be exposed by this container. When provided, property name is used as public name, otherwise public name is automatically inferred from request.
-   */
-  exports?: string[];
-  /**
-   * Container dependent locations and request scopes from which modules should be resolved and loaded at runtime. When provided, property name is used as request scope, otherwise request scope is automatically inferred from container dependent location.
-   */
-  dependencies?: Record<string, DependencyConfig | string>;
-}
 
 export const getPlugins = () => {
   if (process.env.SPA) return [];
