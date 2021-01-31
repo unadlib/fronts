b52ee55e321e31922d5ff8fde174cd3e7cad6650
unadlib <unadlib@gmail.com>
2021-02-01 01:00:15 +0800

    fix(deps): fix deps info injection issue for non-module-federation

--- packages/fronts/src/injectDependencies.ts
+++ packages/fronts/src/injectDependencies.ts
@@ -1,5 +1,12 @@
-window.__FRONTS__ = window.__FRONTS__ ?? {};
-if (!window.__FRONTS__[process.env.APP_NAME!]) {
-  window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(process.env.FPM_DEPS!);
+try {
+  window.__FRONTS__ = window.__FRONTS__ ?? {};
+  if (!window.__FRONTS__[process.env.APP_NAME!]) {
+    window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(
+      process.env.FPM_DEPS!
+    );
+  }
+  window.__FRONTS__.__main__ =
+    window.__FRONTS__.__main__ ?? process.env.APP_NAME;
+} catch (e) {
+  //
 }
-window.__FRONTS__.__main__ = window.__FRONTS__.__main__ ?? process.env.APP_NAME;
