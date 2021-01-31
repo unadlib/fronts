d9e198da81c1e59abae5590c84987477cd62cf93
unadlib <unadlib@gmail.com>
2021-02-01 00:44:33 +0800

    refactor(deps): refactor injectDependencies

--- packages/fronts/src/index.ts
+++ packages/fronts/src/index.ts
@@ -1,8 +1,4 @@
-window.__FRONTS__ = window.__FRONTS__ ?? {};
-if (!window.__FRONTS__[process.env.APP_NAME!]) {
-  window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(process.env.FPM_DEPS!);
-}
-window.__FRONTS__.__main__ = window.__FRONTS__.__main__ ?? process.env.APP_NAME;
+import './injectDependencies';
 
 export * from './loadApp';
 export * from './useApp';
---
+++ packages/fronts/src/injectDependencies.ts
@@ -0,0 +1,5 @@
+window.__FRONTS__ = window.__FRONTS__ ?? {};
+if (!window.__FRONTS__[process.env.APP_NAME!]) {
+  window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(process.env.FPM_DEPS!);
+}
+window.__FRONTS__.__main__ = window.__FRONTS__.__main__ ?? process.env.APP_NAME;
