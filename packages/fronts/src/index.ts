a92930a828de5a9b5a268bef5b3d4a534efc44f5
unadlib <unadlib@gmail.com>
2021-02-01 00:38:05 +0800

    fix(deps): update

--- packages/fronts/src/index.ts
+++ packages/fronts/src/index.ts
@@ -1,3 +1,9 @@
+window.__FRONTS__ = window.__FRONTS__ ?? {};
+if (!window.__FRONTS__[process.env.APP_NAME!]) {
+  window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(process.env.FPM_DEPS!);
+}
+window.__FRONTS__.__main__ = window.__FRONTS__.__main__ ?? process.env.APP_NAME;
+
 export * from './loadApp';
 export * from './useApp';
 export * from './interface';
@@ -6,9 +12,3 @@ export * from './useIFrame';
 export * from './importApp';
 export * from './injectScript';
 export * from './loadScript';
-
-window.__FRONTS__ = window.__FRONTS__ ?? {};
-if (!window.__FRONTS__[process.env.APP_NAME!]) {
-  window.__FRONTS__[process.env.APP_NAME!] = JSON.parse(process.env.FPM_DEPS!);
-}
-window.__FRONTS__.__main__ = window.__FRONTS__.__main__ ?? process.env.APP_NAME;
