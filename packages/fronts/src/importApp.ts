dc4a7c72f5e5d1e9fa1f84a918b02393b9f9b8be
unadlib <unadlib@gmail.com>
2021-02-14 00:14:59 +0800

    fix(version-control): fix version dep cache issue

--- packages/fronts/src/importApp.ts
+++ packages/fronts/src/importApp.ts
@@ -35,6 +35,13 @@ export const getModule = async (name: string, module: string) => {
   return factory();
 };
 
+const setCacheLink = (storageKey: string, depLink: string) => {
+  window.localStorage.setItem(storageKey, depLink);
+};
+
+const getCacheLink = (storageKey: string) =>
+  window.localStorage.getItem(storageKey);
+
 export const importApp = async (path: string) => {
   const [name, ...paths] = path.split('/');
   const modulePath = `./${paths.join('/')}`;
@@ -42,22 +49,24 @@ export const importApp = async (path: string) => {
     try {
       const frontsPackagesMap = JSON.parse(process.env.FPM_MAP);
       const depInfo = frontsPackagesMap[name];
+      const storageKey = `${storePrefix}${name}`;
       const isExternalLink = /^(http|https):/.test(depInfo);
-      const cacheLink = window.localStorage.getItem(`${storePrefix}${name}`);
+      const cacheLink = getCacheLink(storageKey);
       if (isExternalLink) {
         await loadModuleScript(name, depInfo);
       } else if (cacheLink) {
         fetch(`${process.env.FPM_REG}?scope=${name}`).then(async (data) => {
           const depLinks = await data.json();
           const depLink = getDepLink(depLinks[name], depInfo);
-          window.localStorage.setItem(`${storePrefix}${name}`, depLink);
+          setCacheLink(storageKey, depLink);
         });
         await loadModuleScript(name, cacheLink);
       } else {
-        const data: Record<string, Record<string, string>> = await fetch(
+        const depLinks: Record<string, Record<string, string>> = await fetch(
           `${process.env.FPM_REG}?scope=${name}`
         ).then((data) => data.json());
-        const depLink = getDepLink(data[name], depInfo);
+        const depLink = getDepLink(depLinks[name], depInfo);
+        setCacheLink(storageKey, depLink);
         await loadModuleScript(name, depLink);
       }
       const module = await getModule(name, modulePath);
