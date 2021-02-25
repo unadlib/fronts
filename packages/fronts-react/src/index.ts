6c3ba0774293d336e5b1d8624eee065e34189b2d
unadlib <unadlib@gmail.com>
2021-02-26 01:11:38 +0800

    chore(webcomponents): init WebComponents

--- packages/fronts-react/src/index.ts
+++ packages/fronts-react/src/index.ts
@@ -3,3 +3,4 @@ export { boot, loadScript } from 'fronts';
 export * from './useApp';
 export * from './interface';
 export * from './useIFrame';
+export * from './useWebComponents';
--- packages/fronts/src/index.ts
+++ packages/fronts/src/index.ts
@@ -1,10 +1,11 @@
 import './injectDependencies';
 
 export * from './loadApp';
-export * from './useApp';
 export * from './interface';
 export * from './boot';
+export * from './useApp';
 export * from './useIFrame';
+export * from './useWebComponents';
 export * from './importApp';
 export * from './injectScript';
 export * from './loadScript';
