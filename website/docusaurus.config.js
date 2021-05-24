dfa12b78fdda84262261528816348c5783fa66b2
unadlib <unadlib@gmail.com>
2021-05-25 00:26:22 +0800

    docs(website): update docusaurus config

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -25,7 +25,7 @@ module.exports = {
         },
         {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
+          href: 'https://github.com/unadlib/fronts',
           label: 'GitHub',
           position: 'right',
         },
@@ -85,13 +85,13 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            'https://github.com/unadlib/fronts/edit/master/website/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            'https://github.com/unadlib/fronts/edit/master/website/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
