c1d30500fa4d44d9b60379941d6daafbd9210609
unadlib <unadlib@gmail.com>
2022-01-03 23:09:05 +0800

    Deploy website - based on fdac01e200885be68ee52273b8b3e8a6b9993256

---
+++ assets/js/7c06a738.0fe3775d.js
@@ -0,0 +1,1 @@
+(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[711],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"A progressive micro frontends framework - Fronts","permalink":"/blog/A progressive micro frontends framework - Fronts"}]}')}}]);
